cb485e72c4a1281dce40e4679508140c69ad36a0
Flor Lafuente <florlafuente@MBP-de-Wingu.fibertel.com.ar>
2020-03-13 15:06:03 -0300

    Add pagination component

---
+++ frontend/components/List/Table.js
@@ -0,0 +1,91 @@
+const Table = () => {
+  return (
+    <div className="w-full mx-auto">
+      <div className="bg-white shadow-md my-6">
+        <table className="text-left w-full border-collapse">
+          <thead>
+            <tr>
+              <th className="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b border-grey-light">Nombre</th>
+              <th className="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b border-grey-light">Apellido</th>
+              <th className="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b border-grey-light">Tipo</th>
+              <th className="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b border-grey-light">Email</th>
+              <th className="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b border-grey-light">Teléfono</th>
+              <th className="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b border-grey-light">Género</th>
+              <th className="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b border-grey-light">Cuenta</th>
+              <th className="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b border-grey-light"></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="bg-grey-lighter">
+              <td className="py-4 px-6 border-b border-grey-light">Juana</td>
+              <td className="py-4 px-6 border-b border-grey-light">Molina</td>
+              <td className="py-4 px-6 border-b border-grey-light">Molina</td>
+              <td className="py-4 px-6 border-b border-grey-light">Molina</td>
+              <td className="py-4 px-6 border-b border-grey-light">Molina</td>
+              <td className="py-4 px-6 border-b border-grey-light">Molina</td>
+              <td className="py-4 px-6 border-b border-grey-light">Molina</td>
+              <td className="py-4 px-6 border-b border-grey-light">
+                <a href="#" className="text-grey-lighter font-bold py-1 px-3 rounded text-xs bg-green hover:bg-green-dark">Editar</a>
+                <a href="#" className="text-grey-lighter font-bold py-1 px-3 rounded text-xs bg-blue hover:bg-blue-dark">Eliminar</a>
+              </td>
+            </tr>
+            <tr className="bg-grey-lighter">
+              <td className="py-4 px-6 border-b border-grey-light">Juana</td>
+              <td className="py-4 px-6 border-b border-grey-light">Molina</td>
+              <td className="py-4 px-6 border-b border-grey-light">Molina</td>
+              <td className="py-4 px-6 border-b border-grey-light">Molina</td>
+              <td className="py-4 px-6 border-b border-grey-light">Molina</td>
+              <td className="py-4 px-6 border-b border-grey-light">Molina</td>
+              <td className="py-4 px-6 border-b border-grey-light">Molina</td>
+              <td className="py-4 px-6 border-b border-grey-light">
+                <a href="#" className="text-grey-lighter font-bold py-1 px-3 rounded text-xs bg-green hover:bg-green-dark">Editar</a>
+                <a href="#" className="text-grey-lighter font-bold py-1 px-3 rounded text-xs bg-blue hover:bg-blue-dark">Eliminar</a>
+              </td>
+            </tr>
+            <tr className="bg-grey-lighter">
+              <td className="py-4 px-6 border-b border-grey-light">Juana</td>
+              <td className="py-4 px-6 border-b border-grey-light">Molina</td>
+              <td className="py-4 px-6 border-b border-grey-light">Molina</td>
+              <td className="py-4 px-6 border-b border-grey-light">Molina</td>
+              <td className="py-4 px-6 border-b border-grey-light">Molina</td>
+              <td className="py-4 px-6 border-b border-grey-light">Molina</td>
+              <td className="py-4 px-6 border-b border-grey-light">Molina</td>
+              <td className="py-4 px-6 border-b border-grey-light">
+                <a href="#" className="text-grey-lighter font-bold py-1 px-3 rounded text-xs bg-green hover:bg-green-dark">Editar</a>
+                <a href="#" className="text-grey-lighter font-bold py-1 px-3 rounded text-xs bg-blue hover:bg-blue-dark">Eliminar</a>
+              </td>
+            </tr>
+            <tr className="bg-grey-lighter">
+              <td className="py-4 px-6 border-b border-grey-light">Juana</td>
+              <td className="py-4 px-6 border-b border-grey-light">Molina</td>
+              <td className="py-4 px-6 border-b border-grey-light">Molina</td>
+              <td className="py-4 px-6 border-b border-grey-light">Molina</td>
+              <td className="py-4 px-6 border-b border-grey-light">Molina</td>
+              <td className="py-4 px-6 border-b border-grey-light">Molina</td>
+              <td className="py-4 px-6 border-b border-grey-light">Molina</td>
+              <td className="py-4 px-6 border-b border-grey-light">
+                <a href="#" className="text-grey-lighter font-bold py-1 px-3 rounded text-xs bg-green hover:bg-green-dark">Editar</a>
+                <a href="#" className="text-grey-lighter font-bold py-1 px-3 rounded text-xs bg-blue hover:bg-blue-dark">Eliminar</a>
+              </td>
+            </tr>
+            <tr className="bg-grey-lighter">
+              <td className="py-4 px-6 border-b border-grey-light">Juana</td>
+              <td className="py-4 px-6 border-b border-grey-light">Molina</td>
+              <td className="py-4 px-6 border-b border-grey-light">Molina</td>
+              <td className="py-4 px-6 border-b border-grey-light">Molina</td>
+              <td className="py-4 px-6 border-b border-grey-light">Molina</td>
+              <td className="py-4 px-6 border-b border-grey-light">Molina</td>
+              <td className="py-4 px-6 border-b border-grey-light">Molina</td>
+              <td className="py-4 px-6 border-b border-grey-light">
+                <a href="#" className="text-grey-lighter font-bold py-1 px-3 rounded text-xs bg-green hover:bg-green-dark">Editar</a>
+                <a href="#" className="text-grey-lighter font-bold py-1 px-3 rounded text-xs bg-blue hover:bg-blue-dark">Eliminar</a>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
+    </div>
+  )
+}
+
+export default Table
