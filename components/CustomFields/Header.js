b27b2a010f47a5992f64847d27a4c6e84f1d9ebc
Flor Lafuente <lafuente.florencia@gmail.com>
2020-03-20 10:58:36 -0300

    Separate frontend from backend

---
+++ components/CustomFields/Header.js
@@ -0,0 +1,9 @@
+const Header = () => (
+  <header className="w-full flex items-center justify-between">
+    <h1 className="mr-4 inline-block border-b-4 border-yellow-wingu font-semibold text-2xl ">
+      Configuración de campos
+    </h1>
+  </header>
+)
+
+export default Header
