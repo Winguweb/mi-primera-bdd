6512517f43e0ff204340dcdc61d7e5a30cd4866c
Flor Lafuente <lafuente.florencia@gmail.com>
2020-03-20 17:32:42 -0300

    Add svg loader

---
+++ .next/static/chunks/styles.55b297963d5f71b30e6e.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[22],{iOjB:function(n,w,o){}}]);
--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 const withCSS = require('@zeit/next-css')
 module.exports = withCSS({
-  
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      issuer: {
+        test: /\.(js|ts)x?$/,
+      },
+      use: ['@svgr/webpack'],
+    });
+
+    return config;
+  }
 })
