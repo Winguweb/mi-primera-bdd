dfab2e820a32be0ffc051e49e6ecab724ced2696
Flor Lafuente <lafuente.florencia@gmail.com>
2020-04-02 15:47:23 -0300

    Add fields to account mutation

--- graphql/account/mutation/createAccount.js
+++ graphql/account/mutation/createAccount.js
@@ -1,8 +1,19 @@
 import { gql } from "apollo-boost";
 
 export const CREATE_ACCOUNT = gql`
-  mutation CreateAccount($name: String!, $organization: ID!) {
-    createAccount(input: { data: { name: $name, organizacion: $organization} }) {
+  mutation CreateAccount($name: String!, $address: String, $email: String, $alternative_email: String, $phone: Long, $alt_phone: Long, $web: String, $observations: String, $account_type: ID, $organization: ID!) {
+    createAccount(input: { data: { 
+      name: $name,
+      address: $address,
+      email: $email,
+      alternative_email: $alternative_email,
+      phone: $phone,
+      alt_phone: $alt_phone,
+      web: $web,
+      observations: $observations,
+      account_type: $account_type,
+      organizacion: $organization
+    }}) {
       account {
         id
         name
--- graphql/account/mutation/editAccount.js
+++ graphql/account/mutation/editAccount.js
@@ -1,8 +1,19 @@
 import { gql } from "apollo-boost";
 
 export const EDIT_ACCOUNT = gql`
-  mutation UpdateAccount($name: String!) {
-    updateAccount(input: { data:  { name: $name } }) {
+  mutation UpdateAccount($name: String!, $address: String, $email: String, $alternative_email: String, $phone: Long, $alt_phone: Long, $web: String, $observations: String, $account_type: ID, $organization: ID!) {
+    updateAccount(input: { data:  { 
+      name: $name,
+      address: $address,
+      email: $email,
+      alternative_email: $alternative_email,
+      phone: $phone,
+      alt_phone: $alt_phone,
+      web: $web,
+      observations: $observations,
+      account_type: $account_type,
+      organizacion: $organization
+    }}) {
       account {
         id
         name
