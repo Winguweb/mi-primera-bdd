7ac22e4b9bbcd40100fe4d50320ac471713db2e8
Flor Lafuente <lafuente.florencia@gmail.com>
2020-04-08 11:39:30 -0300

    Add some svg icons

---
+++ components/Icons/index.js
@@ -0,0 +1,13 @@
+import Account from './Account'
+import Close from './Close'
+import Delete from './Delete'
+import Edit from './Edit'
+import Logout from './Logout'
+
+export {
+  Account,
+  Close,
+  Delete,
+  Edit,
+  Logout
+}
