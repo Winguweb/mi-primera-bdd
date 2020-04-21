9b717668bce423d4ab04452ca29bce42c0bec260
Flor Lafuente <lafuente.florencia@gmail.com>
2020-04-21 15:54:41 -0300

    Delete repetead field in Contact table

--- pages/contacts.js
+++ pages/contacts.js
@@ -16,9 +16,6 @@ const Contacts = props => {
     { name: 'Apellido',
       key: 'lastname'
     },
-    { name: 'Nombre',
-      key: 'name'
-    },
     { name: 'Tipo',
       key: 'contact_type'
     },
