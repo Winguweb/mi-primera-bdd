e66d782cdeecfc2dcc5684edaf71c479a5efb5f9
gus <gustavo@winguweb.org>
2020-04-29 17:33:33 -0300

    move fields constants to another file

--- pages/account/new.js
+++ pages/account/new.js
@@ -4,52 +4,13 @@ import { CREATE_ACCOUNT } from '../../graphql/account/mutation/createAccount'
 import { DELETE_CONTACT } from '../../graphql/contact/mutation/deleteContact'
 import { DELETE_OPPORTUNITY } from '../../graphql/opportunity/mutation/deleteOpportunity'
 import { getIdFromLocalCookie } from '../../lib/auth'
+import { contactFields, opportunityFields } from './fields'
 import securePage from '../../hocs/securePage'
 import AccountForm from '../../components/AccountForm'
 import Table from '../../components/List/Table'
 
 
 const NewAccount = () => {
-  const contactField = [
-    { name: 'Nombre',
-      key: 'name'
-    },
-    { name: 'Apellido',
-      key: 'lastname'
-    },
-    { name: 'Tipo',
-      key: 'contact_type'
-    },
-    { name: 'Email',
-      key: 'email'
-    },
-    { name: 'Teléfono',
-      key: 'phone'
-    },
-    { name: 'Género',
-      key: 'gender'
-    },
-    { name: 'Cuenta',
-      key: 'account'
-    },
-  ]
-  const Opportunityfields = [
-    { name: 'Nombre de la oportunidad',
-      key: 'name'
-    },
-    { name: 'Estado',
-      key: 'state'
-    },
-    { name: 'Tipo',
-      key: 'opportunity_type'
-    },
-    { name: 'Cuenta',
-      key: 'account'
-    },
-    { name: 'Fecha',
-      key: 'date'
-    },
-  ]
   return (
     <Query query={GET_ACCOUNT_FIELDS}  variables={{organization: getIdFromLocalCookie()}}>
       {({ loading, data, error }) => {
@@ -61,10 +22,8 @@ const NewAccount = () => {
             <AccountForm
               data={data}
               mutation={CREATE_ACCOUNT} />
-            <Table info={data.contacts} workspace="contact" fields={contactField} delete={DELETE_CONTACT} />
-            <Table info={data.opportunities} workspace="opportunity" fields={Opportunityfields} delete={DELETE_OPPORTUNITY} />
-
-
+            <Table info={data.contacts} workspace="contact" fields={contactFields} delete={DELETE_CONTACT} />
+            <Table info={data.opportunities} workspace="opportunity" fields={opportunityFields} delete={DELETE_OPPORTUNITY} />
           </>
         )
       }}
