9773a909b50b38b0af788dcbd2a1c89f1087dadc
gus <gustavo@winguweb.org>
2020-04-30 14:02:15 -0300

    remove field.js file

--- pages/account/new.js
+++ pages/account/new.js
@@ -4,13 +4,53 @@ import { CREATE_ACCOUNT } from '../../graphql/account/mutation/createAccount'
 import { DELETE_CONTACT } from '../../graphql/contact/mutation/deleteContact'
 import { DELETE_OPPORTUNITY } from '../../graphql/opportunity/mutation/deleteOpportunity'
 import { getIdFromLocalCookie } from '../../lib/auth'
-import { contactFields, opportunityFields } from './fields'
 import securePage from '../../hocs/securePage'
 import AccountForm from '../../components/AccountForm'
 import Table from '../../components/List/Table'
 
 
 const NewAccount = () => {
+  const contactFields = [
+    { name: 'Nombre',
+      key: 'name'
+    },
+    { name: 'Apellido',
+      key: 'lastname'
+    },
+    { name: 'Tipo',
+      key: 'contact_type'
+    },
+    { name: 'Email',
+      key: 'email'
+    },
+    { name: 'Teléfono',
+      key: 'phone'
+    },
+    { name: 'Género',
+      key: 'gender'
+    },
+    { name: 'Cuenta',
+      key: 'account'
+    },
+  ]
+  
+  const opportunityFields = [
+    { name: 'Nombre de la oportunidad',
+      key: 'name'
+    },
+    { name: 'Estado',
+      key: 'state'
+    },
+    { name: 'Tipo',
+      key: 'opportunity_type'
+    },
+    { name: 'Cuenta',
+      key: 'account'
+    },
+    { name: 'Fecha',
+      key: 'date'
+    },
+  ]
   return (
     <Query query={GET_ACCOUNT_FIELDS}  variables={{organization: getIdFromLocalCookie()}}>
       {({ loading, data, error }) => {
