48ca88fcb31756722f1abbbc3e842bb0f95670b7
juandans01 <juandans01@gmail.com>
2020-06-18 16:08:43 -0300

    change relations list on accounts from new to show

--- pages/account/[id].js
+++ pages/account/[id].js
@@ -3,11 +3,58 @@ import { Query } from 'react-apollo'
 import { GET_ACCOUNT } from '../../graphql/account/query/getAccount'
 import { EDIT_ACCOUNT } from '../../graphql/account/mutation/editAccount'
 import { getIdFromLocalCookie } from '../../lib/auth'
+import { DELETE_CONTACT } from '../../graphql/contact/mutation/deleteContact'
+import { DELETE_OPPORTUNITY } from '../../graphql/opportunity/mutation/deleteOpportunity'
+import Table from '../../components/List/Table'
 import securePage from '../../hocs/securePage'
 import AccountForm from '../../components/AccountForm'
 
 const Account = (props) => {
   const router= useRouter()
+
+  const contactFields = [
+    { name: 'Nombre',
+      key: 'name'
+    },
+    { name: 'Apellido',
+      key: 'lastname'
+    },
+    { name: 'Tipo',
+      key: 'contact_type'
+    },
+    { name: 'Email',
+      key: 'email'
+    },
+    { name: 'Teléfono',
+      key: 'phone'
+    },
+    { name: 'Género',
+      key: 'gender'
+    },
+    { name: 'Cuenta',
+      key: 'account'
+    },
+  ]
+  
+  const opportunityFields = [
+    { name: 'Nombre de la oportunidad',
+      key: 'name'
+    },
+    { name: 'Estado',
+      key: 'state'
+    },
+    { name: 'Tipo',
+      key: 'opportunity_type'
+    },
+    { name: 'Cuenta',
+      key: 'account'
+    },
+    { name: 'Fecha',
+      key: 'date'
+    },
+  ]
+
+
   return (
     <Query query={GET_ACCOUNT} variables={{ id: router.query.id, organization: getIdFromLocalCookie()}}>
       {({ loading, data, error }) => {
@@ -16,9 +63,14 @@ const Account = (props) => {
         }
 
         return (
-          <AccountForm
-            data={data}
-            mutation={EDIT_ACCOUNT} />
+          <>
+            <AccountForm
+              data={data}
+              mutation={EDIT_ACCOUNT} 
+            />
+            <Table info={data.contacts} workspace="contact" fields={contactFields} delete={DELETE_CONTACT} />
+            <Table info={data.opportunities} workspace="opportunity" fields={opportunityFields} delete={DELETE_OPPORTUNITY} />
+          </>
         )
       }}
     </Query>
--- pages/account/new.js
+++ pages/account/new.js
@@ -1,12 +1,9 @@
 import { Query } from 'react-apollo'
 import { GET_ACCOUNT_FIELDS } from '../../graphql/account/query/getAccountFields'
 import { CREATE_ACCOUNT } from '../../graphql/account/mutation/createAccount'
-import { DELETE_CONTACT } from '../../graphql/contact/mutation/deleteContact'
-import { DELETE_OPPORTUNITY } from '../../graphql/opportunity/mutation/deleteOpportunity'
 import { getIdFromLocalCookie } from '../../lib/auth'
 import securePage from '../../hocs/securePage'
 import AccountForm from '../../components/AccountForm'
-import Table from '../../components/List/Table'
 
 
 const NewAccount = () => {
@@ -61,9 +58,7 @@ const NewAccount = () => {
           <>
             <AccountForm
               data={data}
-              mutation={CREATE_ACCOUNT} />
-            <Table info={data.contacts} workspace="contact" fields={contactFields} delete={DELETE_CONTACT} />
-            <Table info={data.opportunities} workspace="opportunity" fields={opportunityFields} delete={DELETE_OPPORTUNITY} />
+              mutation={CREATE_ACCOUNT} />            
           </>
         )
       }}
