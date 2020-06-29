894a9096671fac1e57441355f304765a2e83ed9f
gus <gustavo@winguweb.org>
2020-06-29 13:50:05 -0300

    add useWindowSize breakpoint

--- pages/login.js
+++ pages/login.js
@@ -5,6 +5,8 @@ import Router from 'next/router'
 import Cookies from 'js-cookie'
 import LoginForm from '../components/LoginForm'
 import Icon from '../assets/wingudata-icon.svg'
+import MobileIcon from '../assets/ilustracion-mobile.svg'
+import { useWindowSize } from '../hooks/useWindowSize'
 
 const Login = (props) => {
   useEffect(() => {
@@ -12,7 +14,7 @@ const Login = (props) => {
       Router.push("/") // redirect if you're already logged in
     }
   }, [])
-
+  const size = useWindowSize()
   const onSubmit = ({ email, password }) => {
     handleLoading(true)
     strapiLogin(email, password).then(() => {
@@ -28,15 +30,17 @@ const Login = (props) => {
   const [loading, handleLoading] = useState(false)
 
   const [errorMessage, handleErrorMessage] = useState(null)
-
+  console.log(size)
   return (
-    <div className="h-screen max-h-screen flex justify-center items-center">
+    <div className="h-screen max-h-screen flex justify-center items-center" id="loginWrapper" >
       <div className="p-x h-screen w-1/4"> 
-        <div className="absolute left-0 top-0 bottom-0 ">
-          <Icon />
+        <div className="absolute left-0 top-0 bottom-0 " id="iconWrapper" >
+          {size.width > 640 && <Icon /> }
+          {size.width <= 640 && <MobileIcon /> }
+
         </div>
       </div>
-      <div className="w-2/4 ml-64">
+      <div className="w-2/4 ml-64" id="loginFormWrapper">
           <LoginForm 
             submit={onSubmit} 
             loading={loading}
